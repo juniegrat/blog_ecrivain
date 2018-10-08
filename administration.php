8d7397a8077bbd71385176319c2267dddd6b546a
Unknown <juniegrat@gmail.com>
2018-10-08 09:41:18 +0200

    Edit administration.php to add a link to add.php

--- administration.php
+++ administration.php
@@ -8,11 +8,10 @@ admin_only();
 
 require 'inc/header.php';
 
-
 require_once 'inc/db.php';
 
 ?>
-    <a href="#" class="btn btn-primary">Ajouter un article</a>
+    <a href="add.php" class="btn btn-primary">Ajouter un article</a>
 
     <table class="table">
         <thead>
@@ -25,26 +24,27 @@ require_once 'inc/db.php';
         </thead>
         <tbody>
         <?php
-        // On récupère les 5 derniers billets
-        $req = $pdo->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM news ORDER BY date_creation DESC');
+// On récupère les 5 derniers billets
+$req = $pdo->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM news ORDER BY date_creation DESC');
 
-        while ($donnees = $req->fetch()) {
-            ?>
+while ($donnees = $req->fetch()) {
+        ?>
             <tr>
-                <th scope="row"><?= $donnees->id; ?></th>
-                <td><?= $donnees->title; ?></td>
-                <td><?= $donnees->date_creation_fr; ?></td>
+                <th scope="row"><?=$donnees->id;?></th>
+                <td><?=$donnees->title;?></td>
+                <td><?=$donnees->date_creation_fr;?></td>
                 <td>
                     <em><a class="btn btn-primary" href="edit.php?billet=<?php echo $donnees->id; ?>">Editer</a></em>
-                    <em><a class="btn btn-danger" href="delete.php?billet=<?php echo $donnees->id; ?>">Supprimer</a></em>
+                    <em><a class="btn btn-danger" href="delete.php?billet=<?php echo $donnees->id; ?>&category=news">Supprimer</a></em>
                 </td>
             </tr>
 
 
-            <?php
-        } // Fin de la boucle des billets
-        $req->closeCursor();
-        ?>
+        <?php
+} // Fin de la boucle des billets
+$req->closeCursor();
+?>
         </tbody>
     </table>
-<?php require 'inc/footer.php'; ?>
+
+<?php require 'inc/footer.php';?>
