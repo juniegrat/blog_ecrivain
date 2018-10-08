bf87c1a42c18d4a8d69fc58dbd22cfca7c0fb24e
Unknown <juniegrat@gmail.com>
2018-10-08 09:38:22 +0200

    edit the index.php to remove unescessary request

--- index.php
+++ index.php
@@ -6,38 +6,21 @@ require 'inc/functions.php';
 
 logged_only();
 
-?>
-
-<?php
-
-require_once  'inc/db.php';
-
-if(!empty($_POST)){
-
-    if(empty($_POST['title'])){
-        $_SESSION['flash']['danger'] = "Veuillez indiquer un titre";
-    }elseif(empty($_POST['content'])){
-
-        $_SESSION['flash']['danger'] = "Veuillez entrer le contenu de l'article";
-    }else{
-        $req = $pdo->prepare('INSERT INTO news SET title = ?, content = ?, date_creation = NOW()');
-
-        $req->execute([$_POST['title'],$_POST['content']]);
-
-        $_SESSION['flash']['success'] = "Le billet à bien été posté";
-    }
-}
-?>
-<?php
+require_once 'inc/db.php';
 
 require 'inc/header.php';
+
+if ($_SESSION['auth']->admin == 1) {
+    ?>
+ <a href="administration.php" class="btn btn-primary">Administration</a>
+<?php
+}
 
 // On récupère les 5 derniers billets
 $req = $pdo->query('SELECT id, title, content, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM news ORDER BY date_creation DESC LIMIT 0, 5');
 
-while ($donnees = $req->fetch())
-{
-?>
+while ($donnees = $req->fetch()) {
+    ?>
 <div class="news">
     <h3>
         <?php echo htmlspecialchars($donnees->title); ?>
@@ -46,8 +29,8 @@ while ($donnees = $req->fetch())
 
     <p>
     <?php
-    // On affiche le contenu du billet
-    echo nl2br(htmlspecialchars($donnees->content));
+// On affiche le contenu du billet
+    echo nl2br($donnees->content);
     ?>
     <br />
     <em><a href="comments.php?billet=<?php echo $donnees->id; ?>">Commentaires</a></em>
@@ -56,17 +39,6 @@ while ($donnees = $req->fetch())
 <?php
 } // Fin de la boucle des billets
 $req->closeCursor();
-?>
-<form action="" method="POST">
-    <div class="form-group">
-        <input class="form-control" type="text" name="title" placeholder="Titre du billet">
-    </div>
-    <div class="form-group">
-        <input class="form-control" type="text" name="content" placeholder="Entrez votre texte">
-    </div>
 
-    <button class="btn btn-primary">Envoyer</button>
-</form>
-
-<?php require 'inc/footer.php'; ?>
+require 'inc/footer.php';?>
 
