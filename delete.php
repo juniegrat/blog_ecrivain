339ba951f46a53fef9453c92346f3e2fd65c91e2
Unknown <juniegrat@gmail.com>
2018-10-08 09:39:02 +0200

    Edit delete.php to make it more reusable

--- delete.php
+++ delete.php
@@ -8,10 +8,18 @@ admin_only();
 
 require_once 'inc/db.php';
 
-$req = $pdo->prepare('DELETE FROM news WHERE id = ?');
+if ($_GET['category'] === 'news') {
+    $req = $pdo->prepare('DELETE FROM news WHERE id = ?');
 
-$req->execute([$_GET['billet']]);
+    $req->execute([$_GET['billet']]);
 
-$_SESSION['flash']['success'] = "L'article à bien été supprimé";
+    $_SESSION['flash']['success'] = "L'article à bien été supprimé";
+} else {
+    $req = $pdo->prepare('DELETE FROM comments WHERE id = ?');
 
+    $req->execute([$_GET['billet']]);
+
+    $_SESSION['flash']['success'] = "Le commentaire à bien été supprimé";
+}
 header('location: administration.php');
+exit();
