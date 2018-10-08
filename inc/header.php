abecce0867acf2f570c506637ede0e48e06126d1
Unknown <juniegrat@gmail.com>
2018-10-08 09:36:59 +0200

    edit header.php to add tinymce

--- inc/header.php
+++ inc/header.php
@@ -20,7 +20,38 @@ if (session_status() == PHP_SESSION_NONE) {
 
     <link rel="stylesheet" href="css/main.css">
 
-    <script src="editor.js"></script>
+   <script src='https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=t5niqycm0fdijcdvb49k04fbkn79lw4x2anllbhx83d4vz2n'></script>
+  <script type="text/javascript">
+        tinymce.PluginManager.add('placeholder', function (editor) {
+            editor.on('init', function () {
+                var label = new Label;
+                onBlur();
+                tinymce.DOM.bind(label.el, 'click', onFocus);
+                editor.on('focus', onFocus);
+                editor.on('blur', onBlur);
+                editor.on('change', onBlur);
+                editor.on('setContent', onBlur);
+                function onFocus() { if (!editor.settings.readonly === true) { label.hide(); } editor.execCommand('mceFocus', false); }
+                function onBlur() { if (editor.getContent() == '') { label.show(); } else { label.hide(); } }
+            });
+            var Label = function () {
+                var placeholder_text = editor.getElement().getAttribute("placeholder") || editor.settings.placeholder;
+                var placeholder_attrs = editor.settings.placeholder_attrs || { style: { position: 'absolute', top: '2px', left: 0, color: '#aaaaaa', padding: '.25%', margin: '5px', width: '80%', 'font-size': '17px !important;', overflow: 'hidden', 'white-space': 'pre-wrap' } };
+                var contentAreaContainer = editor.getContentAreaContainer();
+                tinymce.DOM.setStyle(contentAreaContainer, 'position', 'relative');
+                this.el = tinymce.DOM.add(contentAreaContainer, "label", placeholder_attrs, placeholder_text);
+            }
+            Label.prototype.hide = function () { tinymce.DOM.setStyle(this.el, 'display', 'none'); }
+            Label.prototype.show = function () { tinymce.DOM.setStyle(this.el, 'display', ''); }
+    });
+
+  tinymce.init({
+    selector: '.mytextarea-body',
+    statusbar: false,
+    plugins: ['placeholder'],
+
+  });
+  </script>
 
 </head>
 
@@ -41,13 +72,13 @@ if (session_status() == PHP_SESSION_NONE) {
     <a class="navbar-brand" href="index.php">Vent d'Alaska</a>
     <div id="navbar" class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
-            <?php if(isset($_SESSION['auth'])): ?>
+            <?php if (isset($_SESSION['auth'])): ?>
             <li><a href="logout.php">Se déconnecter</a></li>
 
             <?php else: ?>
             <li><a href="register.php">S'inscrire</a></li>
             <li><a href="login.php">Se connecter</a></li>
-            <?php endif; ?>
+            <?php endif;?>
         </ul>
     </div>
 </nav>
@@ -58,16 +89,16 @@ if (session_status() == PHP_SESSION_NONE) {
 
         <?php foreach ($_SESSION['flash'] as $type => $message): ?>
 
-            <div class="alert alert-<?= $type; ?>">
+            <div class="alert alert-<?=$type;?>">
 
-                <?= $message; ?>
+                <?=$message;?>
 
             </div>
 
-        <?php endforeach; ?>
+        <?php endforeach;?>
 
-    <?php unset($_SESSION['flash']); ?>
+    <?php unset($_SESSION['flash']);?>
 
-    <?php endif; ?>
+    <?php endif;?>
 
 
