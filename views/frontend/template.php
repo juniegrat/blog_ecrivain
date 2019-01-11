a9a6235f66869f502302ad83a2a519a4e41164df
junight <juniegrat@gmail.com>
2019-01-11 15:12:07 +0100

    Moved the header and footer includes to the template
    The header and footer were located in an include folder and called by the template. It made more sense to move them directly inside the template

--- views/frontend/template.php
+++ views/frontend/template.php
@@ -1,5 +1,81 @@
-<?php require './inc/header.php';?>
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <title>Vent d'alska</title>
+
+    <link rel="stylesheet" href="./public/css/app.css">
+
+    <link rel="stylesheet" href="./public/css/main.css">
+
+   <script src='https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=t5niqycm0fdijcdvb49k04fbkn79lw4x2anllbhx83d4vz2n'></script>
+  <script src="./public/js/tinymce.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+
+
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+    </div>
+    <a class="navbar-brand" href="index.php?action=listPosts">Vent d'Alaska</a>
+    <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <?php if (isset($_SESSION['auth'])): ?>
+            <li><a href="index.php?action=logout">Se déconnecter</a></li>
+
+            <?php else: ?>
+            <li><a href="index.php?action=register">S'inscrire</a></li>
+            <li><a href="index.php?action=loggin">Se connecter</a></li>
+            <?php endif;?>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+
+    <?php if (isset($_SESSION['flash'])): ?>
+
+        <?php foreach ($_SESSION['flash'] as $type => $message): ?>
+
+            <div class="alert alert-<?=$type;?>">
+
+                <?=$message;?>
+
+            </div>
+
+        <?php endforeach;?>
+
+    <?php unset($_SESSION['flash']);?>
+
+    <?php endif;?>
+
+
+
 
         <?=$content?>
 
-<?php require './inc/footer.php';?>
+</div>
+</body>
+</html>
